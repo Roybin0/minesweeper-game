cd27da7fc82fa7415a5fb5563522720d87762b45
Robyn Smith <robyn.roybin@gmail.com>
2022-09-13 12:21:27 +0000

    Add validation to check correct difficulty level input

--- run.py
+++ run.py
@@ -1,16 +1,33 @@
 import random 
 
+
 def get_difficulty_level():
     """
-    Ask the user what difficulty they would like to play
+    Ask the player what difficulty they would like to play
     """
 
     print("What difficulty would you like to play?")
     print("Type 'E' for easy, 'M' for medium or 'H' for hard")
 
     level = input("Enter difficulty level here: \n")
+ 
+    validate_level(level)
 
-    print(f"You entered {level}")
+def validate_level(data):
+    """
+    Inside the try, converts data input to lowercase.
+    Raises ValueError if string does not match a difficulty level.
+    """
+    try:
+        data = data.lower()
+        if data != 'e':
+            if data != 'm':
+                if data != 'h':
+                    raise ValueError(
+                        f"Expected 'E', 'M', or 'H', you entered {data}"
+                    )
+    except ValueError as e:
+        print(f"Invalid data: {e}")
+
 
 get_difficulty_level()
-
