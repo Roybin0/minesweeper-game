00d906a58cd1d0e2cf1b451c12382025f2157a60
Robyn Smith <robyn.roybin@gmail.com>
2022-09-13 10:33:13 +0000

    Create get_difficulty_level function and get input from player

--- run.py
+++ run.py
@@ -1,3 +1,16 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random 
+
+def get_difficulty_level():
+    """
+    Ask the user what difficulty they would like to play
+    """
+
+    print("What difficulty would you like to play?")
+    print("Type 'E' for easy, 'M' for medium or 'H' for hard")
+
+    level = input("Enter difficulty level here: \n")
+
+    print(f"You entered {level}")
+
+get_difficulty_level()
+
